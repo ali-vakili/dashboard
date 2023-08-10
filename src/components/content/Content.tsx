b1b054e49edd4fcb5451da674f37201082f50439
ali-vakili <ali.vakili.tb@gmail.com>
2023-08-10 22:15:31 +0330

    change content cards column

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -10,25 +10,25 @@ const Content : React.FC = () => {
     <>
       <h5 className="mb-3">Dashboard</h5>
       <div className="row">
-        <div className="col-md-3 px-2">
+        <div className="col-md-6 col-xl-3 px-2">
           <div className={`${styles.card} mb-3`}>
             <h6 className={styles.title}>today</h6>
             <h6>{formattedDate}</h6>
           </div>
         </div>
-        <div className="col-md-3 px-2">
+        <div className="col-md-6 col-xl-3  px-2">
           <div className={`${styles.card} mb-3`}>
             <h6 className={styles.title}>projects</h6>
             <h6>5 Published</h6>
           </div>
         </div>
-        <div className="col-md-3 px-2">
+        <div className="col-md-6 col-xl-3  px-2">
           <div className={`${styles.card} mb-3`}>
             <h6 className={styles.title}>contact</h6>
             <h6>5 New Requests</h6>
           </div>
         </div>
-        <div className="col-md-3 px-2">
+        <div className="col-md-6 col-xl-3  px-2">
           <div className={`${styles.card} mb-3`}>
             <h6 className={styles.title}>clients</h6>
             <h6>15 Successful Clients</h6>
